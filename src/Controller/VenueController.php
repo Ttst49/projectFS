508bd947acada7738915d553e7383b4d3b82edc5
Ttst <thibautstachnick@gmail.com>
2024-04-17 16:47:48 +0200

    add method to edit venue

--- src/Controller/VenueController.php
+++ src/Controller/VenueController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Venue;
 use App\Repository\VenueRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Node\Expr\New_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,7 +31,7 @@ class VenueController extends AbstractController
             "content"=>$venue,
             "code"=>200
         ];
-        return $this->json($response);
+        return $this->json($response,200,[],["groups"=>"venue"]);
     }
 
     #[Route("/create")]
@@ -41,6 +42,20 @@ class VenueController extends AbstractController
         $manager->flush();
         $response = [
             "content" => $newVenue,
+            "code"=>201
+        ];
+        return $this->json($response,200,[],["groups"=>"venue"]);
+    }
+
+
+    #[Route("/edit/{id}",name: "venue_edit")]
+    public function editOne(Venue $venue,SerializerInterface $serializer, Request $request, EntityManagerInterface $manager):Response{
+        $editedVenue = $serializer->deserialize($request->getContent(),Venue::class,"json",["object_to_populate"=>$venue]);
+        $editedVenue->setCreatedAt(new \DateTime());
+        $manager->persist($editedVenue);
+        $manager->flush();
+        $response = [
+            "content" => $editedVenue,
             "code"=>201
         ];
         return $this->json($response,200,[],["groups"=>"venue"]);
