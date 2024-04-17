45ac7b9713f1439562bef7810d207470e6729b3d
Ttst <thibautstachnick@gmail.com>
2024-04-17 22:13:43 +0200

    add route name and a method to get unique artist

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -12,8 +12,8 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route("/api/artist")]
 class ArtistController extends AbstractController
 {
-    #[Route('/index', name: 'app_artist')]
-    public function index(ProfileRepository $repository,EntityManagerInterface $manager): Response
+    #[Route('/index', name: 'app_artist_index')]
+    public function index(ProfileRepository $repository): Response
     {
 
        $artists = $repository->findBy(["role"=>"artist"]);
@@ -22,5 +22,16 @@ class ArtistController extends AbstractController
            "code"=>200
        ];
        return $this->json($response,200,[],["groups"=>"artist"]);
+    }
+
+
+    #[Route('/show/{id}',name: "app_artist_show")]
+    public function showOne(ProfileRepository $repository, $id):Response{
+        $artist = $repository->findBy(["id"=>$id]);
+        $response = [
+            "content"=>$artist,
+            "code"=>200
+        ];
+        return $this->json($response);
     }
 }
