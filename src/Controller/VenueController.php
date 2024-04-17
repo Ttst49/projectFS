65ccb5beccc7da686ff1a115baaef1b02c104d77
Ttst <thibautstachnick@gmail.com>
2024-04-17 16:41:01 +0200

    add method to create new venue

--- src/Controller/VenueController.php
+++ src/Controller/VenueController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Venue;
 use App\Repository\VenueRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route("/api/venue")]
 class VenueController extends AbstractController
@@ -31,7 +34,15 @@ class VenueController extends AbstractController
     }
 
     #[Route("/create")]
-    public function createOne():Response{
-
+    public function createOne(SerializerInterface $serializer,Request $request, EntityManagerInterface $manager):Response{
+        $newVenue = $serializer->deserialize($request->getContent(),Venue::class,"json");
+        $newVenue->setOwner($this->getUser()->getProfile());
+        $manager->persist($newVenue);
+        $manager->flush();
+        $response = [
+            "content" => $newVenue,
+            "code"=>201
+        ];
+        return $this->json($response,200,[],["groups"=>"venue"]);
     }
 }
